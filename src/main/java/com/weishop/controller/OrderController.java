abca558b5b3ebfb1a10277a658fc1712cc2b959e
a497556016 <heshaowei_code@163.com>
2017-11-06 16:03:51 +0800

    222222

--- src/main/java/com/weishop/controller/OrderController.java
+++ src/main/java/com/weishop/controller/OrderController.java
@@ -1,13 +1,27 @@
 package com.weishop.controller;
 
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
+import com.baomidou.mybatisplus.plugins.Page;
+import com.google.common.collect.Lists;
 import com.weishop.base.BaseController;
+import com.weishop.base.BaseResponse;
+import com.weishop.base.PageResponse;
+import com.weishop.dto.OrderDTO;
+import com.weishop.global.ResponseCode;
 import com.weishop.pojo.Order;
+import com.weishop.pojo.OrderList;
+import com.weishop.service.IOrderListService;
 import com.weishop.service.impl.OrderServiceImpl;
-
-import org.springframework.stereotype.Controller;
+import com.weishop.utils.PropertyUtils;
 
 /**
  * <p>
@@ -17,8 +31,42 @@ import org.springframework.stereotype.Controller;
  * @author HeShaowei
  * @since 2017-10-17
  */
-@Controller
+@RestController
 @RequestMapping("//order")
 public class OrderController extends BaseController<OrderServiceImpl, Order> {
+	@Autowired
+	private IOrderListService orderListService;
 	
+	@RequestMapping("/createOrder")
+	@ResponseBody
+	public BaseResponse<OrderDTO> createOrder(@RequestBody OrderDTO orderDto){
+		this.baseService.createOrder(orderDto);
+		return BaseResponse.result(orderDto);
+	}
+	
+	@RequestMapping("/selectUserOrders")
+	@ResponseBody
+	public PageResponse<OrderDTO> selectUserOrders(Page<Order> page){
+		PageResponse<OrderDTO> pageResponse = new PageResponse<>();
+		EntityWrapper<Order> orderWrapper = getRequestMapToWrapper();
+		orderWrapper.orderBy(Order.CREATE_TIME, false);
+		page = this.baseService.selectPage(page, orderWrapper);
+		List<OrderDTO> orderDTOs = Lists.newArrayList();
+		for(Order order : page.getRecords()) {
+			OrderDTO orderDTO = new OrderDTO();
+			orderDTO.setOrder(order);
+			EntityWrapper<OrderList> wrapper = new EntityWrapper<>();
+			wrapper.eq(OrderList.ORDER_ID, order.getId());
+			List<OrderList> orderLists = orderListService.selectList(wrapper);
+			orderDTO.setOrderList(orderLists);
+			
+			orderDTOs.add(orderDTO);
+		}
+		pageResponse.setRows(orderDTOs);
+		pageResponse.setCode(ResponseCode.SUCCESS.getCode());
+		pageResponse.setPageNo(page.getCurrent());
+		pageResponse.setPageSize(page.getSize());
+		pageResponse.setTotal(page.getTotal());
+		return pageResponse;
+	}
 }	
--- src/main/java/com/weishop/service/impl/OrderServiceImpl.java
+++ src/main/java/com/weishop/service/impl/OrderServiceImpl.java
@@ -1,9 +1,18 @@
 package com.weishop.service.impl;
 
 import com.weishop.pojo.Order;
+import com.weishop.pojo.OrderList;
+import com.weishop.dto.OrderDTO;
+import com.weishop.mapper.OrderListMapper;
 import com.weishop.mapper.OrderMapper;
+import com.weishop.mapper.ShopCartMapper;
 import com.weishop.service.IOrderService;
 import com.baomidou.mybatisplus.service.impl.ServiceImpl;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -16,5 +25,26 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class OrderServiceImpl extends ServiceImpl<OrderMapper, Order> implements IOrderService {
+	@Autowired
+	private OrderListMapper orderListMapper;
+	@Autowired
+	private ShopCartMapper shopCartMapper;
+	@Override
+	public void createOrder(OrderDTO orderDto) {
+		Order order = orderDto.getOrder();
+		order.setCreateTime(new Date());
+		this.baseMapper.insert(order);
+		List<OrderList> orderList = orderDto.getOrderList();
+		for (OrderList ol : orderList) {
+			ol.setOrderId(order.getId());
+			orderListMapper.insert(ol);
+		}
+		
+		//删除购物车数据
+		int i = shopCartMapper.deleteBatchIds(orderDto.getDelShopCartIds());
+		if(i!=orderDto.getDelShopCartIds().size()) {
+			throw new RuntimeException("删除购物车数据异常，应删除"+orderDto.getDelShopCartIds().size()+"条，实际删除"+i+"条！s");
+		}
+	}
 	
 }
